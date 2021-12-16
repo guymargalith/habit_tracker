98c45bec480f89fb58e7ea2747356dbedf4b4fa2
“cairnsy6” <cairns.rhys@gmail.com>
2021-12-16 09:31:47 +0000

    Added some responsive CSS

--- client/static/js/layout.js
+++ client/static/js/layout.js
@@ -1,16 +1,7 @@
-// const loginSection = document.querySelector("#login-section");
-// const registerSection = document.querySelector("#register-section");
-// const loginButton = document.querySelector("#login-button");
-// const registerButton = document.querySelector("#register-button");
+const {renderHomepage, renderLoginForm, renderRegisterForm, renderUserHabitsPage, render404} = require('./content')
+
 const mainSection = document.querySelector("#main-section");
 
-
-// renderHomepage();
-
-// renderUserHabitsPage();
-
-const publicRoutes = ['#', '#login', '#register'];
-const privateRoutes = ['#userhabits'];
 
 window.addEventListener('hashchange', updateMain)
 window.addEventListener('load', updateMain)
@@ -19,7 +10,6 @@ function updateMain(){
     const path = window.location.hash
     mainSection.innerHTML ='';
     if(localStorage.getItem('token')){
-        // window.location.hash = '#user-habits'
         renderUserHabitsPage();
     } else {
         switch (path){
@@ -38,64 +28,3 @@ function updateMain(){
     }
 }
 }
-
-// function updateMain(){
-//     const path = window.location.hash
-//     if( path === '#'){
-//         renderHomepage();
-//     }
-//     else if(path === '#login'){
-//         renderLoginForm()
-//     }
-// }
-
-// updateMain()
-
-// function updateMain(path) {
-//     mainSection.innerHTML = '';
-//     if (path) {
-//         switch(path){
-//             case '#login':
-//                 renderLoginForm(); break;
-//             case '#register':
-//                 renderRegisterForm(); break;
-//             // case '#feed':
-//             //     renderFeed(); break;
-//             // case '#profile':
-//             //     renderProfile(); break;
-//             // default:
-//             //     render404(); break;
-//         }
-//     } else {
-//         renderHomepage();
-//     }
-// }
-
-// function createButtonLink(route){
-//     console.log(route)
-//     const button = document.createElement('button')
-//     button.classList = 'btn btn-lg btn-danger'
-//     console.log(`${route[1]}${route.substring(2)}`)
-//     button.textContent = route === '#' ? 'Home' : `${route[1].toUpperCase()}${route.substring(2)}`;
-//     button.href = route;
-//     return button;
-// }
-
-// function updateContent(){
-//     const path = window.location.path
-//     console.log(path)
-//     updateMain(path);
-// }
-
-// function updateContent(){
-//     const path = window.location.hash;
-//     if (privateRoutes.includes(path) && !currentUser()){
-//         window.location.hash = '#';
-//     } else if (!privateRoutes.includes(path) && currentUser()) {
-//         window.location.hash = '#feed';
-//     } else {
-//         updateMain(path);
-//     }
-// }
-
-// updateContent()
